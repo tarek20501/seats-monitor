2a7bee2e810cedfaa703d1e165b9cf4fc6628ca3
Tarek Ayoubi <tarek20501@hotmail.com>
2021-06-05 21:29:33 -0700

    object orentied refactor

--- SeatsMonitor.py
+++ SeatsMonitor.py
@@ -4,13 +4,17 @@ from urllib.request import urlopen as uReq
 from datetime import datetime
 import time
 from notify_run import Notify
+import copy
+
+notify = Notify()
+courses = list()
 
 class Seats:
     def __init__(self):
-        self.totalSeatsRemaining = -1
-        self.currentlyRegistered = -1
-        self.generalSeatsRemaining = -1
-        self.restrictedSeatsRemaining = -1
+        self.totalSeatsRemaining = '-1'
+        self.currentlyRegistered = '-1'
+        self.generalSeatsRemaining = '-1'
+        self.restrictedSeatsRemaining = '-1'
 
 class Course:
     def __init__(self, year, session, department, course, section):
@@ -22,46 +26,44 @@ class Course:
         self.currentSeats = Seats()
         self.previousSeats = Seats()
 
-def getCourseInfo(year, session, dep, course, section):
-    link  = 'https://courses.students.ubc.ca/cs/courseschedule?sesscd=' + session
-    link += '&pname=subjarea&tname=subj-section&sessyr=' + year + '&course=' + course
-    link += '&section=' + section + '&dept=' + dep
-    try:
-        uClient = uReq(link, None, 5.0)
-        page_html = uClient.read()
-        uClient.close()
-        page_soap = soap(page_html, 'html.parser')
-        
-        SeatSum = page_soap.body.find(class_="'table").find_all('tr')
-        total      = SeatSum[0]
-        registered = SeatSum[1]
-        general    = SeatSum[2]
-        restricted = SeatSum[3]
+    def updateSeats(self):
+        link  = 'https://courses.students.ubc.ca/cs/courseschedule?sesscd=' + self.session
+        link += '&pname=subjarea&tname=subj-section&sessyr=' + self.year + '&course=' + self.course
+        link += '&section=' + self.section + '&dept=' + self.department
+        try:
+            uClient = uReq(link, None, 5.0)
+            page_html = uClient.read()
+            uClient.close()
+            page_soap = soap(page_html, 'html.parser')
+            SeatSum = page_soap.body.find(class_="'table").find_all('tr')
+            total      = SeatSum[0]
+            registered = SeatSum[1]
+            general    = SeatSum[2]
+            restricted = SeatSum[3]
 
-        dateTimeObj = datetime.now()
-        print(year + ' ' + session + ' ' + dep + ' ' + course + ' ' + section)
-        print('\t' + total.td.get_text() + ' ' + total.strong.get_text())
-        print('\t' + registered.td.get_text() + ' ' + registered.strong.get_text())
-        print('\t' + general.td.get_text() + ' ' + general.strong.get_text())
-        print('\t' + restricted.td.get_text() + ' ' + restricted.strong.get_text())
-        print('[' + dateTimeObj.strftime('%T') + ']')
-    except:
-        print('Something went wrong in bs4...')
-        return [-1]
-    
-    return [total.strong.get_text(), registered.strong.get_text(), general.strong.get_text(), restricted.strong.get_text()]
+            self.previousSeats = copy.deepcopy(self.currentSeats)
+            self.currentSeats.totalSeatsRemaining      = total.strong.get_text()
+            self.currentSeats.currentlyRegistered      = registered.strong.get_text()
+            self.currentSeats.generalSeatsRemaining    = general.strong.get_text()
+            self.currentSeats.restrictedSeatsRemaining = restricted.strong.get_text()
 
-def compare(course, curr, prev):
-        notify = Notify()
-        if (curr[2] != prev[2]):
-            message = course[2] + ' ' + course[3] + ': \n'
-            # message += 'Total Seats Remaining: ' + str(prev[0]) + ' -> ' + str(curr[0]) + '\n'
-            # message += 'Currently Registered:' + str(prev[1]) + ' -> ' + str(curr[1]) + '\n'
-            message += 'General Seats Remaining: ' + str(prev[2]) + ' -> ' + str(curr[2]) + '\n'
-            # message += 'Restricted Seats Remaining*: ' + str(prev[3]) + ' -> ' + str(curr[3]) + '\n'
-            notify.send(message)            
+            print(self.year + ' ' + self.session + ' ' + self.department + ' ' + self.course + ' ' + self.section)
+            print('\t' + total.td.get_text()      + ' ' + self.currentSeats.totalSeatsRemaining)
+            print('\t' + registered.td.get_text() + ' ' + self.currentSeats.currentlyRegistered)
+            print('\t' + general.td.get_text()    + ' ' + self.currentSeats.generalSeatsRemaining)
+            print('\t' + restricted.td.get_text() + ' ' + self.currentSeats.restrictedSeatsRemaining)
+            dateTimeObj = datetime.now()
+            print('[' + dateTimeObj.strftime('%T') + ']')
+        except:
+            print('Something went wrong in bs4...')
+            return [-1]
 
-courses = list()
+    def notify(self):
+        message = self.department + ' ' + self.course + ': \n'
+        if (self.previousSeats.generalSeatsRemaining != self.currentSeats.generalSeatsRemaining):
+            message += 'General Seats Remaining: ' + self.previousSeats.generalSeatsRemaining + ' -> ' + self.currentSeats.generalSeatsRemaining + '\n'
+            notify.send(message)
+            print("notification sent!")
 
 def addCourse():
     # TODO
@@ -77,38 +79,29 @@ def saveCourses():
 
 def loadCourses():
     # <YEAR> <W/S> <DEPARTMENT> <COURSE> <SECTION>
+    #   0      1        2          3         4
     global courses
     courses_file = open('courses.txt', 'r')
     courses = courses_file.readlines()
     courses = list(map(str.strip, courses))
-    # courses = list(map(str.split, courses))
-    # for i in range(len(courses)):
-        # courses[i] = Course(courses[i][0], courses[i][1], courses[i][2], courses[i][3], courses[i][4])
+    courses = list(map(str.split, courses))
+    for i in range(len(courses)):
+        courses[i] = Course(courses[i][0], courses[i][1], courses[i][2], courses[i][3], courses[i][4])
     courses_file.close()
 
-def process(period, notify):
-    if notify == 'y':
-        prevInfo = [[0,0,0,0]] * len(courses)
-
+def process(period, isNotify):
     while True:
-        i = 0
         for course in courses:
             print('--------------------------------------')
-            course = course.split(' ')
-            currInfo = getCourseInfo(course[0], course[1], course[2], course[3], course[4])
-            if currInfo[0] == -1:
-                i += 1
-                continue
-            if notify == 'y':
-                compare(course, currInfo, prevInfo[i])
-                prevInfo[i] = currInfo
-            i += 1
+            course.updateSeats()
+            if isNotify == 'y':
+                course.notify()
         print('--------------------------------------')
         time.sleep(period)
 
 if __name__ == "__main__":
     loadCourses()
-    notify = ' '
-    while (notify != 'y' and notify != 'n'):
-        notify = input('Do you want notifications? (y or n) ')
-    process(1, notify)
+    isNotify = ' '
+    while (isNotify != 'y' and isNotify != 'n'):
+        isNotify = input('Do you want notifications? (y or n) ')
+    process(1, isNotify)
